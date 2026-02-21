9f1345ce05ce1f5c07e0079419033600bd1372cc
RamirezJM <165516814+RamirezJM@users.noreply.github.com>
2026-02-20 23:24:12 -0300

    feat: agregar rutas

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,37 @@
 import {createRouter, createWebHistory} from 'vue-router'
+import Home from '../views/Home.vue'
+import Catalog from '../views/Catalog.vue'
+import Product from '../views/Product.vue'
+import About from '../views/About.vue' 
 
-const routes = []
+const routes = [
+  {
+    path: '/',
+    name: 'home',
+    component: Home
+  },
+  {
+    path: '/catalog',
+    name: 'catalog',
+    component: Catalog
+  },
+  {
+    path: '/product/:id',
+    name: 'product',
+    component: Product,
+    props: true
+  },
+  {
+    path: '/inicio',
+    redirect: '/'
+  },
+  {
+    path: '/about',
+    name: 'about',
+    component: About,
+    alias: '/acerca'
+  }
+]
 
 const router = createRouter({
   history: createWebHistory(),
